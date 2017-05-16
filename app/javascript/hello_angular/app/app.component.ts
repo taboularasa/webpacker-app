5679210a92a4998584713d12b147d453313a2035
David Elliott <david.elliott@stitchfix.com>
2017-05-16 13:41:20 -0700

    initial

---
+++ app/javascript/hello_angular/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-angular',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  name = 'Angular!';
+}
